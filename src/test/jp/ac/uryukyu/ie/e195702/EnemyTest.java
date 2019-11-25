5925d248b5d4d67d81a37ad14976b1abf38e0db4
Ryo Okuma <e195702@ie.u-ryukyu.ac.jp>
2019-11-26 00:22:21 +0900

    ステップ１

--- src/test/jp/ac/uryukyu/ie/e195702/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e195702/EnemyTest.java
@@ -15,7 +15,11 @@ public class EnemyTest {
         Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.dead = true;
         for(int i=0; i<10; i++) {
-            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+            if (enemy.dead = true){
+                break;
+            }else{
+                enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+            }
         }
         assertEquals(heroHP, hero.hitPoint);
     }
